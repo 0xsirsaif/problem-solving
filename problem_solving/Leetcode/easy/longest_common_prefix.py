35fb67ec506ad539da4951b3bcae5e8b510b7df9
sir saif <sirsaif99@gmail.com>
2020-07-11 13:02:26 +0200

    11/7/2020

--- problem_solving/Leetcode/easy/longest_common_prefix.py
+++ problem_solving/Leetcode/easy/longest_common_prefix.py
@@ -9,31 +9,37 @@ class Solution:
                 if other[i] != char:
                     return shortest_char[:i]
         return shortest_char
-        # def longestCommonPrefix(self, strs):
-    #     def is_perfix_exist(perfix):
-    #         for s in strs:
-    #             if perfix not in s:
-    #                 return False
-    #         return True
-    #     if strs:
-    #         if len(strs) == 1:
-    #             return strs[0]
-    #         x = 0
-    #         y = 1
-    #         shortest_len = min([len(s) for s in strs])
-    #         perfixes = []
-    #         while y <= shortest_len:
-    #             perfix = strs[0][x:y]
-    #             if is_perfix_exist(perfix):
-    #                 perfixes.append(perfix)
-    #                 y += 1
-    #             else:
-    #                 x += 1
-    #                 y += 1
-    #         if perfixes:
-    #             pass
-    #     return ""
+
 
 
 s = Solution()
-print(s.longestCommonPrefix(["ac","c"]))
+print(s.longestCommonPrefix(["c","ac"]))
+
+
+def is_perfix_exist(perfix):
+    for s in strs:
+        if perfix not in s:
+            return False
+    return True
+
+
+# if strs:
+#     if len(strs) == 1:
+#         return strs[0]
+#     x = 0
+#     y = 1
+#     shortest_len = max([len(s) for s in strs])
+#     perfixes = []
+#     while y <= shortest_len:
+#         perfix = strs[0][x:y]
+#         if is_perfix_exist(perfix):
+#             perfixes.append(perfix)
+#             y += 1
+#         else:
+#             x += 1
+#             y += 1
+#     if perfixes:
+#         return perfixes[-1]
+# return ""
+
+
