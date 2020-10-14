76a86e66cac088066e89b20a26b6c6fb2943a7ee
sir saif <sirsaif99@gmail.com>
2020-10-14 18:38:26 +0200

    implement insertion sort algorithm

--- several_problems/insertion_sort_python_list.py
+++ several_problems/insertion_sort_python_list.py
@@ -1,3 +1,20 @@
+# Algorithm 1 By shifting
+def shifting_insertion_sort(arr):
+    for i in range(1, len(arr)):
+        hand = arr[i]
+        j = i-1
+        while j >= 1 and arr[j] > hand:
+            arr[j+1] = arr[j]
+            j -= 1
+        else:
+            if hand > arr[j]:
+                arr[j+1] = hand
+            else:
+                arr[j+1] = arr[j]
+                arr[j] = hand
+    return arr
+
+# Algorithm 2 using binary search
 def insertion_sort(arr):
     i = 0
     while i < len(arr)-1:
@@ -8,7 +25,7 @@ def insertion_sort(arr):
     return arr
 
 def find_position(arr, num):
-    start, end, idx = 0, len(arr)-1, None
+    start, end = 0, len(arr)-1
     while start <= end:
         mid = (start+end)//2
         if arr[mid] == num:
