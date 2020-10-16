b2ac61d038e9c5a8a17a7c1686a8b137c1b63858
sir saif <sirsaif99@gmail.com>
2020-10-16 15:58:16 +0200

    accepted solution for 34 leetcode problem

--- Leetcode/34_first_and_last_idx.py
+++ Leetcode/34_first_and_last_idx.py
@@ -1,18 +1,27 @@
 from collections import Counter
+from typing import List
 
-def find_target(arr, start, end, target):
-    if start > end-1:
-        return False
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def _binary_search(arr, start, end, num):
+            if start > end:
+                return [-1, -1]
 
-    mid = (start + end) // 2
+            mid = (start + end) // 2
 
-    if arr[mid] == target:
-        return mid
-    elif target > arr[mid]:
-        return find_target(arr, mid + 1, end, target)
-    elif target < arr[mid]:
-        return find_target(arr, start, mid - 1, target)
+            if arr[mid] == num:
+                counter = dict(Counter(arr))
+                before_target = sum([counter[x] for x in counter if x < num])
+                after_target = len(arr) - before_target - counter[num]
+                return [before_target, len(arr) - after_target - 1]
+            elif num > arr[mid]:
+                return _binary_search(arr, mid + 1, end, num)
+            elif num < arr[mid]:
+                return _binary_search(arr, start, mid - 1, num)
+
+        return _binary_search(nums, 0, len(nums)-1, target)
 
 
-A = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-print(find_target(A, 0, len(A), 1))
+A = [1]
+s = Solution()
+print(s.searchRange(A, 1))
