22224791cda6f4a202910d517c2c4249ccacf9e6
sir saif <sirsaif99@gmail.com>
2020-10-22 16:36:17 +0200

    simple changes

--- data_structures_and_algorithms_in_python_book/array-based_sequence_ch5/R-5.2.py
+++ data_structures_and_algorithms_in_python_book/array-based_sequence_ch5/R-5.2.py
@@ -4,11 +4,13 @@ def list_size(n):
     A = []
     reallocate = -1
     for k in range(n):
+        arr_len = len(A)
         arr_memory_usage = getsizeof(A)
         num_elements = (arr_memory_usage - 64)//8
         if num_elements != reallocate:
             print(num_elements)
             reallocate = num_elements
+        print("Length: {0: 3d}; Size in bytes: {1: 4d}".format(arr_len, arr_memory_usage))
         A.append(None)
 
 list_size(30)
