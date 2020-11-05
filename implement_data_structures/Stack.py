7a3c1986d4ebb7ba58d906df63bcbad5fab3c226
sir saif <sirsaif99@gmail.com>
2020-11-05 17:16:42 +0200

    implement Array-based stack data structure

---
+++ implement_data_structures/Stack.py
@@ -0,0 +1,23 @@
+class Empty(Exception):
+    pass
+
+
+class Stack:
+    def __init__(self):
+        self._data = []
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, element):
+        self._data.append(element)
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty("Empty Stack")
+        return self._data.pop()
+
+    def top(self):
+        if self.is_empty():
+            raise Empty("Empty Stack")
+        return self._data[-1]
