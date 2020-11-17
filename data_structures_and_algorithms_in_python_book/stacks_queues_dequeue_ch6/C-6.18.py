b9856e2a4bc212c80a6257b4e975bdbcaca59c2d
sir saif <sirsaif99@gmail.com>
2020-11-17 10:06:17 +0200

    reverse Stack using two temporary stacks.

--- data_structures_and_algorithms_in_python_book/stacks_queues_dequeue_ch6/C-6.18.py
+++ data_structures_and_algorithms_in_python_book/stacks_queues_dequeue_ch6/C-6.18.py
@@ -22,3 +22,29 @@ class Stack:
             raise Empty("Empty Stack")
         return self._data[-1]
 
+    def print_stack(self):
+        print(">>", self._data)
+
+
+def transfer(s, t):
+    while not s.is_empty():
+        t.push(s.pop())
+    return t
+
+
+def reverse_stack(S, A, B):
+    transfer(transfer(transfer(S, A), B), S)
+
+S = Stack()
+tem_A = Stack()
+temp_B = Stack()
+
+S.push(1)
+S.push(2)
+S.push(3)
+S.push(4)
+S.push(5)
+
+S.print_stack()
+reverse_stack(S, tem_A, temp_B)
+S.print_stack()
