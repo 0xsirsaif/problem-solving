d740d0438ea144cb7c11c2bce7457f5c2ba1a56d
sir saif <sirsaif99@gmail.com>
2020-11-19 23:07:07 +0200

    1658 leetcode problem

---
+++ Leetcode/1658_minimum_operations_to_reduce_X_to_zero.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        def _recur(arr, idx, result):
+            if not arr:
+                pass
+            elif len(arr) == 1:
+                pass
+            elif result == 0:
+                pass
+
+        if not nums:
+            return -1
