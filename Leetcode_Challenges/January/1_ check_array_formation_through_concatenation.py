491e754e3125f8be256f3beee86ffe9e0222d9f7
sir saif <sirsaif99@gmail.com>
2021-01-05 21:18:35 +0200

    Jan Challenge - Day1

--- Leetcode_Challenges/January/1_ check_array_formation_through_concatenation.py
+++ Leetcode_Challenges/January/1_ check_array_formation_through_concatenation.py
@@ -1,6 +1,7 @@
 from typing import List
 from itertools import chain
 
+
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
         map = {x[0]: x for x in pieces}
