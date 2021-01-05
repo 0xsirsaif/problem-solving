8f6bd7befc0a37c24b25c06f1e032575a01750fd
sir saif <sirsaif99@gmail.com>
2021-01-05 21:11:16 +0200

    uncompleted problem

--- Leetcode/1658_minimum_operations_to_reduce_X_to_zero.py
+++ Leetcode/1658_minimum_operations_to_reduce_X_to_zero.py
@@ -3,11 +3,13 @@ from typing import List
 
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        def _recur():
-            pass
+        def _recur(arr, node, right, n_steps, result, flag):
+            print(node)
+            if node < len(arr):
+                pass
+            return -1
 
-        steps = []
-        _recur()
+        _recur(nums, 0, len(nums)-1, 1, x, False)
         return 0
 
 
