6bf669a6274371f3354363b051c8f5bfa8d72538
sir saif <sirsaif99@gmail.com>
2021-01-06 15:17:46 +0200

    Jan Challenge - Day5 - completed

--- Leetcode_Challenges/January/5_ remove_duplicates_from_sorted_list_II.py
+++ Leetcode_Challenges/January/5_ remove_duplicates_from_sorted_list_II.py
@@ -6,36 +6,28 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if not head:
-            return None
+        dummy = curr = ListNode(-1, head)
 
-        dummy = curr = ListNode(0)
         while head:
             if head.next and head.val == head.next.val:
-                # reach the last node
-                # reach the last in duplicated nodes
+                # loop till the last duplicated node
                 while head.next and head.val == head.next.val:
                     head = head.next
+                curr.next = head.next
             else:
-                curr.next = head
+                # confirmed to be distinct
                 curr = curr.next
-
             head = head.next
-
-        s = dummy.next
-        while s:
-            print(">>", s.val)
-            s = s.next
 
         return dummy.next
 
 
 n7 = ListNode(5)
-n6 = ListNode(4, n7)
+n6 = ListNode(4)
 n5 = ListNode(4, n6)
-n4 = ListNode(3, n5)
-n3 = ListNode(2)
-n2 = ListNode(2, n3)
+n4 = ListNode(2, n5)
+n3 = ListNode(2, n4)
+n2 = ListNode(1, n3)
 n1 = ListNode(1, n2)
 
 obj = Solution()
