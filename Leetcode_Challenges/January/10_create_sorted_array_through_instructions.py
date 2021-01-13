4a6c89852c0849ca2e02b4f650b392b6db56174e
sir saif <sirsaif99@gmail.com>
2021-01-13 10:34:48 +0200

    day 10 Jan Challenge - Fenwick Tree - uncompleted

---
+++ Leetcode_Challenges/January/10_create_sorted_array_through_instructions.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+A = [1,2,3,4,5]
+
+
+class Solution:
+    def createSortedArray(self, instructions: List[int]) -> int:
+        pass
+
+S = Solution()
+print(S.createSortedArray([]))
