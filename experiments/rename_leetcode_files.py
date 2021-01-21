0961f8ede02cfbd8ca735ff6eeaad8b3b469cf68
sir saif <sirsaif99@gmail.com>
2021-01-21 07:52:47 +0200

    rename files

--- experiments/rename_leetcode_files.py
+++ experiments/rename_leetcode_files.py
@@ -2,4 +2,4 @@ def rename(problem_address: str) -> str:
     return problem_address.replace(" ", "_").lower()
 
 
-print(rename("15 Valid Parentheses"))
+print(rename("19 Longest Palindromic Substring"))
