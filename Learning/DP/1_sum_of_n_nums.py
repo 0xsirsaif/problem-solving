912f812c8cd45cd4d8a19457d9a4edb035c3c1fe
sir saif <sirsaif99@gmail.com>
2021-01-21 11:55:05 +0200

    simple optimization

--- Learning/DP/1_sum_of_n_nums.py
+++ Learning/DP/1_sum_of_n_nums.py
@@ -1,8 +1,9 @@
 # find summation of n integers
 def n_sum(n):
-    if n == 0:
-        return 0
     summation = [0]
-    for i in range(1, n+1):
-        summation.append(summation[i-1] + i)
+    for i in range(1, n + 1):
+        summation.append(summation[i - 1] + i)
     return summation[-1]
+
+
+print(n_sum(0))
